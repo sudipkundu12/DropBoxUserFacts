5926ea8e332d67e54e5d0f817196ab85e8b19d24
sudip kundu <kundu.sudip1994@gmail.com>
2020-12-11 11:12:26 +0530

    fixed issues for ios 12 compatible

--- DropBoxUserFacts/AppDelegate/AppDelegate.swift
+++ DropBoxUserFacts/AppDelegate/AppDelegate.swift
@@ -12,7 +12,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: FactsListTableViewController())
+        (UIApplication.shared.delegate as? AppDelegate)?.self.window = window
         return true
     }
 
